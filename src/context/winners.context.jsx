ce1bbc334cb04634164d9241422dbcc97e9bbc80
suyamaed <suyama_eduardo@redlink.com.ar>
2022-12-02 12:39:42 -0300

    datos mokeadosen gnadores

--- src/context/winners.context.jsx
+++ src/context/winners.context.jsx
@@ -1,11 +1,27 @@
 import { createContext, useState } from 'react';
 
+
 export const WinnerContext = createContext({
     
 });
 
 export const WinnersProvider = ({children}) => {
-    const [winners, setWinners] = useState();
+    const [winners, setWinners] = useState([
+        {
+            id: 1,
+            nombre: "Juan",
+            apellido: "Perez",
+            sorteo: 1,
+            premio: "15mil"
+        },
+        {
+            id: 111,
+            nombre: "Marisa",
+            apellido: "Alvarez",
+            sorteo: 5,
+            premio: "40mil"
+        }
+    ]);
     const value = {winners};
 
     return(
--- src/routes/lottery/lottery.component.jsx
+++ src/routes/lottery/lottery.component.jsx
@@ -1,18 +1,35 @@
 import NamesLoop from '../../components/namesloop/namesloop.component';
 
+import { useContext } from 'react';
+import { WinnerContext } from '../../context/winners.context';
+
 import { 
     LotteryContainer,
     Title 
 } from './lottery.styles';
 
 const Lottery = () => {
+    const {winners} = useContext(WinnerContext);
+    console.log("Winners:" + winners.length);
     return(
         <div>
             <Title>SORTEO LINK</Title>
             <LotteryContainer>
                 <NamesLoop/>
             </LotteryContainer>
+            <div style={{color: "black"}}>
+            {
+                winners.length ? (winners.map(winner=>(
+                    <div key={winner.id}>
+                        <span>{winner.nombre}</span>
+                        | 
+                        <span>{winner.apellido}</span>
+                        | 
+                        <span>{winner.premio}</span>
+                    </div>))) : (<span>No Winner yet</span>)
+            }
         </div>
+    </div>
     );
 }
 
